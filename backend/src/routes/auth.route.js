f512b2da36d44a7b630edaadf114e3003800f956
Ritik Gupta <>
2026-03-23 20:59:03 +0530

    authentication

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -3,9 +3,9 @@ import { signup, login, logout } from "../controllers/auth.controller.js";
 
 const router = express.Router()
 
-router.get("/login",login);
-router.get("/logout",logout);
-router.get("/signup",signup);
+router.post("/login",login);
+router.post("/logout",logout);
+router.post("/signup",signup);
 
 export default router;
     
